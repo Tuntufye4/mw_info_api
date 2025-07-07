1546fe497e78229553a4437f3706be0c16156b3f
Tuntufye4 <tuntumwanyongo@gmail.com>
2025-07-07 05:31:09 +0200

    updated main urls

--- malawi_api/urls.py
+++ malawi_api/urls.py
@@ -1,9 +1,21 @@
+from django.http import JsonResponse
 from django.contrib import admin
 from django.urls import path, include
 
+def home(request):
+    return JsonResponse({
+        "message": "MW INFO API",
+        "available_endpoints": [
+            "/api/mwinfo/",
+            "/api/agriinfo/",
+            "/api/currency/",
+        ]
+    })
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/mwinfo/", include("mwinfo.urls")),
-    path("api/agriinfo/", include("agriinfo.urls")),
-    path("api/currency/", include("currency.urls")),
+    path('', home),  # 👈 Root route
+    path('admin/', admin.site.urls),
+    path('api/mwinfo/', include('mwinfo.urls')),
+    path('api/agriinfo/', include('agriinfo.urls')),
+    path('api/currency/', include('currency.urls')),
 ]

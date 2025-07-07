d58656cb816217348d6236a041010192de7c9de1
Tuntufye4 <tuntumwanyongo@gmail.com>
2025-07-08 01:28:28 +0200

    added corheaders

--- malawi_api/settings.py
+++ malawi_api/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',  
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+     
+
+CORS_ALLOWED_ORIGINS = [
+    "https://tuntufye4.github.io",
+]
+
 
 ROOT_URLCONF = 'malawi_api.urls'
 
